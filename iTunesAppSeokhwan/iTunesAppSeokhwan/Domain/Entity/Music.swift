2e68b059f6c6408f88db01cd3e95d1e0e330e5f4
youseokhwan <youseokhwan15@gmail.com>
2025-05-08 14:35:09 +0900

    feat: Music Entity 정의

---
+++ iTunesAppSeokhwan/iTunesAppSeokhwan/Domain/Entity/Music.swift
@@ -0,0 +1,21 @@
+//
+//  Music.swift
+//  iTunesAppSeokhwan
+//
+//  Created by youseokhwan on 5/8/25.
+//
+
+import Foundation
+
+struct Music {
+    let id: Int
+    let title: String
+    let artist: String
+    let albumTitle: String
+    let genre: String
+    let releaseDate: Date
+    let runningTime: Int
+    let albumThumbnailImagePath: String
+    let albumOriginalImagePath: String
+    let previewAudioPath: String
+}
