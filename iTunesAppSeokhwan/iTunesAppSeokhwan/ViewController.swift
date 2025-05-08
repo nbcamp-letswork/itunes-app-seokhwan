5a7d82c6db98ef197d83c99d0c8460fa6803c27a
youseokhwan <youseokhwan15@gmail.com>
2025-05-08 12:52:47 +0900

    chore: 프로젝트 생성

---
+++ iTunesAppSeokhwan/iTunesAppSeokhwan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesAppSeokhwan
+//
+//  Created by youseokhwan on 5/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
