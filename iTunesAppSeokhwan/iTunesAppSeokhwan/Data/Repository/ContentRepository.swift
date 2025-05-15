390e4091332bb59e4209b1d25e6d3f4fbd9cabdf
youseokhwan <youseokhwan15@gmail.com>
2025-05-15 19:27:08 +0900

    refactor: 불필요한 코드, 파일 삭제

--- iTunesAppSeokhwan/iTunesAppSeokhwan/Data/Repository/ContentRepository.swift
+++ iTunesAppSeokhwan/iTunesAppSeokhwan/Data/Repository/ContentRepository.swift
@@ -75,40 +75,4 @@ final class ContentRepository {
             withTemplate: "",
         )
     }
-
-    func fetchMusic(for term: String) -> Single<[Music]> {
-        Single.create { [weak self] single in
-            guard let self else { return Disposables.create() }
-
-            Task {
-                let result = await self.service.fetchMusic(for: term)
-
-                switch result {
-                case .success(let response):
-                    single(.success(self.makeMusic(from: response)))
-                case .failure(let error):
-                    single(.failure(DomainError.networkError(error.localizedDescription)))
-                }
-            }
-
-            return Disposables.create()
-        }
-    }
-
-    private func makeMusic(from response: [MusicResponse]) -> [Music] {
-        response.map {
-            Music(
-                id: $0.trackID,
-                title: $0.trackName,
-                artist: $0.artistName,
-                albumTitle: $0.collectionName,
-                genre: $0.primaryGenreName,
-                releaseDate: Date(iso8601String: $0.releaseDate),
-                runningTime: $0.trackTimeMillis,
-                albumImagePath: $0.artworkURL,
-                previewAudioPath: $0.previewURL,
-            )
-        }
-    }
 }
-
