bc0148ad37f05c436fe80565597e5e75687e13ac
youseokhwan <youseokhwan15@gmail.com>
2025-05-19 00:19:00 +0900

    feat: URLCache 적용

--- iTunesAppSeokhwan/iTunesAppSeokhwan/Presentation/Utility/ImageLoader.swift
+++ iTunesAppSeokhwan/iTunesAppSeokhwan/Presentation/Utility/ImageLoader.swift
@@ -6,13 +6,29 @@
 //
 
 import Foundation
+import os
 
 enum ImageLoader {
     static func imageData(from path: String) async -> Data? {
-        guard let url = URL(string: path),
-              let data = try? await URLSession.shared.data(from: url).0 else {
+        guard let url = URL(string: path) else {
             return nil
         }
+
+        let request = URLRequest(
+            url: url,
+            cachePolicy: .returnCacheDataElseLoad,
+        )
+
+        if let cacheResponse = URLCache.shared.cachedResponse(for: request) {
+            os_log("캐시 이미지 로드: \(url.absoluteString)")
+            return cacheResponse.data
+        }
+
+        guard let data = try? await URLSession.shared.data(from: url).0 else {
+            return nil
+        }
+
+        os_log("네트워크 이미지 로드: \(url.absoluteString)")
         return data
     }
 }
